4d250bebc0826f8526fb5de5820443e507a9e049
NguyenLongDang1999 <nguyenlongdang04121999@gmail.com>
2023-05-13 14:32:23 +0700

    Validate

--- validations/attribute.validate.ts
+++ validations/attribute.validate.ts
@@ -1,11 +1,36 @@
 // ** Types Imports
 import type { FormRules } from 'element-plus'
 
+const field = {
+    name: 'Tiêu đề thuộc tính',
+    description: 'Mô tả',
+    category_id: 'Danh mục'
+}
+
 export default {
     name: [
         {
             required: true,
-            message: 'Please input',
+            message: `${field.name} không được bỏ trống.`,
+            trigger: 'change'
+        },
+        {
+            max: 30,
+            message: `${field.name} tối đa 30 ký tự.`,
+            trigger: 'change'
+        }
+    ],
+    category_id: [
+        {
+            required: true,
+            message: `${field.category_id} không được bỏ trống.`,
+            trigger: 'change'
+        }
+    ],
+    description: [
+        {
+            max: 160,
+            message: `${field.description} tối đa 160 ký tự.`,
             trigger: 'change'
         }
     ]
--- validations/category.validate.ts
+++ validations/category.validate.ts
@@ -1,11 +1,52 @@
 // ** Types Imports
 import type { FormRules } from 'element-plus'
 
+const field = {
+    name: 'Tiêu đề danh mục',
+    description: 'Mô tả',
+    meta_title: 'Meta Title',
+    meta_keyword: 'Meta Keyword',
+    meta_description: 'Meta Description'
+}
+
 export default {
     name: [
         {
             required: true,
-            message: 'Please input',
+            message: `${field.name} không được bỏ trống.`,
+            trigger: 'change'
+        },
+        {
+            max: 60,
+            message: `${field.name} tối đa 60 ký tự.`,
+            trigger: 'change'
+        }
+    ],
+    description: [
+        {
+            max: 160,
+            message: `${field.description} tối đa 160 ký tự.`,
+            trigger: 'change'
+        }
+    ],
+    meta_title: [
+        {
+            max: 60,
+            message: `${field.meta_title} tối đa 60 ký tự.`,
+            trigger: 'change'
+        }
+    ],
+    meta_keyword: [
+        {
+            max: 60,
+            message: `${field.meta_keyword} tối đa 60 ký tự.`,
+            trigger: 'change'
+        }
+    ],
+    meta_description: [
+        {
+            max: 160,
+            message: `${field.meta_description} tối đa 160 ký tự.`,
             trigger: 'change'
         }
     ]
